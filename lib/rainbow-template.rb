23ca4519457589bb576d9a1414256c8b22d070dd
Poga Po <poga.bahamut@gmail.com>
2012-12-03 22:56:48 +0800

    remove deprecated codes

--- lib/rainbow-template.rb
+++ lib/rainbow-template.rb
@@ -9,7 +9,3 @@ require "rainbow-template/parser"
 require "rainbow-template/engine"
 
 require "rainbow-template/filters/staticize_incorrect_block_hierarchy"
-
-require "rainbow-template/utils/context_proxy"
-require "rainbow-template/utils/helper_proxy"
-
